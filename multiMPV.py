b926233f7435936d122214e607960d902f3582f8
Robert Spektor <goodfella47@gmail.com>
2021-03-18 22:29:14 +0200

    registered .mmpv format

--- multiMPV.py
+++ multiMPV.py
@@ -4,17 +4,18 @@ from tkinter import Tk
 from tkinter.filedialog import askopenfilenames, askopenfilename
 from tkinter import messagebox
 import configparser
-from shutil import copytree
 import subprocess
 
 FILE_EXTENSION = ".mmpv"
 
 
 class multiMPV:
-    def __init__(self):
-        self.__config_file = 'config.ini'
+    def __init__(self, cwd):
+        self.__cwd = cwd
+        self.__config_file_path = os.path.join(cwd, 'config.ini')
+        self.__first_run = False
         self.__config = configparser.ConfigParser()
-        self.__config.read(self.__config_file)
+        self.__config.read(self.__config_file_path)
         self.__filetypes = [
             ("video file", FILE_EXTENSION),
             ("video file", ".mp4"),
@@ -25,6 +26,11 @@ class multiMPV:
             ("video file", ".AVI"),
             ("video file", ".txt")
         ]
+        if 'first_run' in self.__config['MPV']:
+            self.__first_run = True
+            with open(self.__config_file_path, 'w') as conf:
+                self.__config.remove_option('MPV','first_run')
+                self.__config.write(conf)
         video_scale = self.__config['MPV']['video_scale']
         force_original_aspect_ratio = self.__config['MPV']['force_original_aspect_ratio']
         self.__video_scale = f'scale={video_scale}:force_original_aspect_ratio={force_original_aspect_ratio},pad={video_scale}:-1:-1:color=black'
@@ -83,11 +89,18 @@ class multiMPV:
         if 'MPV' in self.__config and 'mpv_path' in self.__config['MPV']:
             mpv_path = self.__config['MPV']['mpv_path']
 
-        while not os.path.exists(mpv_path):
-            mpv_path = mpv_file_picker()
-            with open(self.__config_file, 'w') as conf:
-                self.__config['MPV']['mpv_path'] = mpv_path
-                self.__config.write(conf)
+        if self.__first_run:
+            while not os.path.exists(mpv_path):
+                mpv_path = mpv_file_picker()
+                with open(self.__config_file_path, 'w') as conf:
+                    self.__config['MPV']['mpv_path'] = mpv_path
+                    self.__config.write(conf)
+            sys.exit()
+
+        elif not os.path.exists(mpv_path):
+            warning = f'mpv executable not found. please change the "mpv_path" in the config.ini file'
+            messagebox.showwarning(title="Exception", message=warning)
+            sys.exit()
 
         mpv_dir = os.path.dirname(mpv_path)
         return mpv_dir
@@ -100,38 +113,36 @@ class multiMPV:
 
     def _get_mpv_command(self, vids):
         input_count = len(vids)
+        input_name = f"input\\input{input_count}.conf"
+        input_conf = f'--input-conf="{os.path.join(self.__cwd,input_name)}"'
+        script_name = f"scripts\\cycle-commands.lua"
+        script = f'--script="{os.path.join(self.__cwd, script_name)}"'
         generated_scale = self._generate_scale(input_count)
         external_file_stack = '"' + ';'.join(vids[1:]) + '"'
 
         if len(vids) == 1:
             mpv_command = f' "{vids[0]}"'
         elif len(vids) == 2:
-            mpv_command = f' --profile="two" --lavfi-complex="[vid1]{self.__video_scale}[v1];[vid2]{self.__video_scale}[v2];[v1][v2]hstack[vo]" "{vids[0]}" --external-file={external_file_stack}'
+            mpv_command = f' {script} {input_conf} --lavfi-complex="[vid1]{self.__video_scale}[v1];[vid2]{self.__video_scale}[v2];[v1][v2]hstack[vo]" "{vids[0]}" --external-file={external_file_stack}'
         elif len(vids) == 3:
-            mpv_command = f' --profile="three" --lavfi-complex="{generated_scale}[v1][v2][v3]xstack=inputs=3:layout=0_0|w0_0|0_h0:fill=black[vo]" "{vids[0]}" --external-files={external_file_stack}'
+            mpv_command = f' {script} {input_conf} --lavfi-complex="{generated_scale}[v1][v2][v3]xstack=inputs=3:layout=0_0|w0_0|0_h0:fill=black[vo]" "{vids[0]}" --external-files={external_file_stack}'
         elif len(vids) == 4:
-            mpv_command = f' --profile="four" --lavfi-complex="{generated_scale}[v1][v2]hstack=inputs=2[top];[v3][v4]hstack=inputs=2[bottom];[top][bottom]vstack=inputs=2[vo]" "{vids[0]}" --external-files={external_file_stack}'
+            mpv_command = f' {script} {input_conf} --lavfi-complex="{generated_scale}[v1][v2]hstack=inputs=2[top];[v3][v4]hstack=inputs=2[bottom];[top][bottom]vstack=inputs=2[vo]" "{vids[0]}" --external-files={external_file_stack}'
         elif len(vids) == 5:
-            mpv_command = f' --profile="five" --lavfi-complex="{generated_scale}[v1][v2][v3][v4][v5]xstack=inputs=5:layout=0_0|w0_0|w0+w1_0|0_h0|w0_h0|w0+w1_h0:fill=black[vo]" "{vids[0]}" --external-files={external_file_stack}'
+            mpv_command = f' {script} {input_conf} --lavfi-complex="{generated_scale}[v1][v2][v3][v4][v5]xstack=inputs=5:layout=0_0|w0_0|w0+w1_0|0_h0|w0_h0|w0+w1_h0:fill=black[vo]" "{vids[0]}" --external-files={external_file_stack}'
         elif len(vids) == 6:
-            mpv_command = f' --profile="six" --lavfi-complex="{generated_scale}[v1][v2][v3]hstack=inputs=3[top];[v4][v5][v6]hstack=inputs=3[bottom];[top][bottom]vstack=inputs=2[vo]" "{vids[0]}" --external-files={external_file_stack}'
+            mpv_command = f' {script} {input_conf} --lavfi-complex="{generated_scale}[v1][v2][v3]hstack=inputs=3[top];[v4][v5][v6]hstack=inputs=3[bottom];[top][bottom]vstack=inputs=2[vo]" "{vids[0]}" --external-files={external_file_stack}'
         elif len(vids) == 7:
-            mpv_command = f' --profile="seven" --lavfi-complex="{generated_scale}[v1][v2][v3][v4][v5][v6][v7]xstack=inputs=7:layout=0_0|w0_0|w0+w1_0|0_h0|w0_h0|w0+w1_h0|0_h0+h1|w0_h0+h1|w0+w1_h0+h1:fill=black[vo]" "{vids[0]}" --external-files={external_file_stack}'
+            mpv_command = f' {script} {input_conf} --lavfi-complex="{generated_scale}[v1][v2][v3][v4][v5][v6][v7]xstack=inputs=7:layout=0_0|w0_0|w0+w1_0|0_h0|w0_h0|w0+w1_h0|0_h0+h1|w0_h0+h1|w0+w1_h0+h1:fill=black[vo]" "{vids[0]}" --external-files={external_file_stack}'
         elif len(vids) == 8:
-            mpv_command = f' --profile="eight" --lavfi-complex="{generated_scale}[v1][v2][v3][v4][v5][v6][v7][v8]xstack=inputs=8:layout=0_0|w0_0|w0+w1_0|0_h0|w0_h0|w0+w1_h0|0_h0+h1|w0_h0+h1|w0+w1_h0+h1:fill=black[vo]" "{vids[0]}" --external-files={external_file_stack}'
+            mpv_command = f' {script} {input_conf} --lavfi-complex="{generated_scale}[v1][v2][v3][v4][v5][v6][v7][v8]xstack=inputs=8:layout=0_0|w0_0|w0+w1_0|0_h0|w0_h0|w0+w1_h0|0_h0+h1|w0_h0+h1|w0+w1_h0+h1:fill=black[vo]" "{vids[0]}" --external-files={external_file_stack}'
         else:
-            mpv_command = f' --profile="nine" --lavfi-complex="{generated_scale}[v1][v2][v3][v4][v5][v6][v7][v8][v9]xstack=inputs=9:layout=0_0|w0_0|w0+w1_0|0_h0|w0_h0|w0+w1_h0|0_h0+h1|w0_h0+h1|w0+w1_h0+h1[vo]" "{vids[0]}" --external-files={external_file_stack}'
+            mpv_command = f' {script} {input_conf} --lavfi-complex="{generated_scale}[v1][v2][v3][v4][v5][v6][v7][v8][v9]xstack=inputs=9:layout=0_0|w0_0|w0+w1_0|0_h0|w0_h0|w0+w1_h0|0_h0+h1|w0_h0+h1|w0+w1_h0+h1[vo]" "{vids[0]}" --external-files={external_file_stack}'
         return mpv_command
-
-    @staticmethod
-    def copy_portable_config(mpv_path):
-        copytree('portable_config', os.path.join(mpv_path, 'portable_config'))
 
     def run(self, txt_file=None):
         Tk().withdraw()  # keep the root window from appearing
         mpv_path = self._assert_mpv_exe()
-        if not os.path.exists(os.path.join(mpv_path, 'portable_config')):
-            self.copy_portable_config(mpv_path)
         if not txt_file:
             vids = self.get_vids()
         else:
@@ -139,11 +150,15 @@ class multiMPV:
         self.assert_vids_location(vids)
         mpv_command = self._get_mpv_command(vids)
         cmd_command = os.path.join(mpv_path, 'mpv.exe') + mpv_command
+        print(cmd_command)
         subprocess.run(cmd_command)
 
 
 if __name__ == '__main__':
-    multi_mpv = multiMPV()
+    cwd = sys.path[0]
+    if cwd.endswith(".zip"):
+        cwd = os.path.dirname(cwd)
+    multi_mpv = multiMPV(cwd)
     if len(sys.argv) != 2:
         multi_mpv.run()
     else:
